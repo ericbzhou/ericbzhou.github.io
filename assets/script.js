8a6151fd14ed69b696368bea4f42e33fbab1fc59
Eric Zhou <ericbzhou0513@gmail.com>
2023-07-12 14:29:36 -0400

    updated

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,18 @@ window.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
+
+window.addEventListener("DOMContentLoaded", function() {
+    var collapsibleTitles = document.getElementsByClassName("header-title");
+    for (var i = 0; i < collapsibleTitles.length; i++) {
+        collapsibleTitles[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+});
