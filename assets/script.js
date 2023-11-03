a2dc8ff02f62c4717a5b6b73449f02197ff701fd
Eric Zhou <ericbzhou0513@gmail.com>
2023-11-03 14:18:37 -0400

    yes

--- assets/script.js
+++ assets/script.js
@@ -15,15 +15,20 @@ window.addEventListener("DOMContentLoaded", function() {
 
 window.addEventListener("DOMContentLoaded", function() {
     var collapsibleTitles = document.getElementsByClassName("header-title");
+    
     for (var i = 0; i < collapsibleTitles.length; i++) {
-        collapsibleTitles[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.display === "block") {
-                content.style.display = "none";
-            } else {
-                content.style.display = "block";
-            }
-        });
+        var content = collapsibleTitles[i].nextElementSibling;
+        content.style.display = "none"; // Initially set the content to be hidden
+
+        collapsibleTitles[i].addEventListener("click", function(contentElement) {
+            return function() {
+                this.classList.toggle("active");
+                if (contentElement.style.display === "block") {
+                    contentElement.style.display = "none";
+                } else {
+                    contentElement.style.display = "block";
+                }
+            };
+        }(content));
     }
 });
